4910df94d525821bcd8ef991c71866eac7afa0e4
Toluwaloope <71833008+Toluwaloope@users.noreply.github.com>
2022-06-26 00:54:19 +0100

    Create setup_config.sh

---
+++ deployment_script/setup_config.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+cd /etc/apache2/sites-available/
+
+sudo cat > /etc/apache2/sites-available/url-monitor.conf << EOF
+<VirtualHost *:80>
+ServerAdmin admin@localhost
+ServerName example.com
+ServerAlias www.example.com
+DocumentRoot /var/www/url-monitor
+ErrorLog ${APACHE_LOG_DIR}/error.log
+CustomLog ${APACHE_LOG_DIR}/access.log combined
+</VirtualHost>
+
+EOL
+
+sudo a2ensite url-monitor.conf
+
+sudo cat > /etc/systemd/system/url-monitor.conf << EOF
+[Unit]
+Description= Url monitor service with .NET Web app running on Ubuntu
+
+[Service]
+WorkingDirectory=/var/www/url-monitor
+ExecStart= sudo dotnet /var/www/url-monitor/urlmonitor.dll
+Restart=always
+RestartSec=10
+KillSignal=SIGINT
+SyslogIdentifier=url-monitor
+User=apache
+Environment=ASPNETCORE-ENVIRONEMT=Development
+
+[Install]
+WantedBy=multi-user.target
+
+EOL
+
+
+
