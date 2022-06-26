8fd994e7c7bcb4d3087ceb90375c53cd23ac1833
Toluwaloope <71833008+Toluwaloope@users.noreply.github.com>
2022-06-26 04:54:40 +0100

    Update setup_apache.sh

--- deployment_script/setup_apache.sh
+++ deployment_script/setup_apache.sh
@@ -4,5 +4,5 @@ sudo apt-get update -y
 sudo apt-get install apache2 -y
 sudo mkdir /var/www/url-monitor
 sudo chown -R $(whoami):$(whoami) /var/www/url-monitor/
-sudo chmod +x -R /var/www/url-monitor/*
+sudo chmod +x -R /var/www/url-monitor/
 
