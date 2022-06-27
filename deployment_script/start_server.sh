095ac6dec9dfd40c9d38af83d53f30ff2d3ca3d1
Toluwaloope <71833008+Toluwaloope@users.noreply.github.com>
2022-06-27 13:14:37 +0100

    added stdout logs to start server script

--- deployment_script/start_server.sh
+++ deployment_script/start_server.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-sudo systemctl start url-monitor.service
+sudo systemctl start url-monitor.service &>> $HOME/codeploylogs.log
 
-sudo systemctl start apache2
+sudo systemctl start apache2 &>> $HOME/codeploylogs.log
 
-sudo systemctl enable url-monitor.service
+sudo systemctl enable url-monitor.service &>> $HOME/codeploylogs.log
 
-sudo systemctl enable apache2
+sudo systemctl enable apache2 &>> $HOME/codeploylogs.log
