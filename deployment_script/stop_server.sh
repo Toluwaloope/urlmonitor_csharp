36fe3950a67d97c00db849ed725d416624705200
Toluwaloope <71833008+Toluwaloope@users.noreply.github.com>
2022-06-27 13:14:03 +0100

    added stdout logs to stop_server script

--- deployment_script/stop_server.sh
+++ deployment_script/stop_server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sudo systemctl stop apache2
+sudo systemctl stop apache2 &>> $HOME/codeploylogs.log
 
-sudo systemctl stop url-monitor.service
+sudo systemctl stop url-monitor.service &>> $HOME/codeploylogs.log
 
-chmod +x *.sh
+chmod +x *.sh &>> $HOME/codeploylogs.log
