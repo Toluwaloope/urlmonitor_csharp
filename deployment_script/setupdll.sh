088aa806ca11be5f6ded2694fc01765e48543ae4
Toluwaloope <71833008+Toluwaloope@users.noreply.github.com>
2022-06-28 09:03:44 +0100

    Update setupdll.sh

--- deployment_script/setupdll.sh
+++ deployment_script/setupdll.sh
@@ -3,3 +3,4 @@
 cd /var/www/url-monitor &>> $HOME/codeploylogs.log
 cp /home/ubuntu/* /var/www/url-monitor &>> $HOME/codeploylogs.log
 unzip *.zip &>> /home/ubuntu/codeploylogs.log
+chown -R $(whoami):$(whoami) /var/www/url-monitor/*
