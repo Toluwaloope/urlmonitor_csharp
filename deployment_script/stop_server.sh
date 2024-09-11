bcc0c3f8a54f792457f3c66cf342da16dfa887de
Toluwaloope <71833008+Toluwaloope@users.noreply.github.com>
2024-09-11 03:46:36 +0100

    removed service stop command

--- deployment_script/stop_server.sh
+++ deployment_script/stop_server.sh
@@ -2,6 +2,6 @@
 
 sudo systemctl stop apache2 &>> $HOME/codeploylogs.log
 
-sudo systemctl stop url-monitor.service &>> $HOME/codeploylogs.log
+#sudo systemctl stop url-monitor.service &>> $HOME/codeploylogs.log
 
 chmod +x *.sh &>> $HOME/codeploylogs.log
